b29a74cc1c99e897513cad67f0af41ef0f48ba82
Christian Chiarulli <chris.machine@pm.me>
2023-05-21 14:00:49 -0400

    feat: show icon and line number

--- lua/telescope/_extensions/filemarks.lua
+++ lua/telescope/_extensions/filemarks.lua
@@ -1,6 +1,7 @@
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local conf = require("telescope.config").values
+local webdevicon = require("nvim-web-devicons")
 
 -- local actions = require("telescope.actions")
 -- local action_state = require("telescope.actions.state")
@@ -21,12 +22,28 @@ local get_file_list = function()
 			table.insert(file_list, { file.path, file.lnum, file.sign_id, file.projects })
 		end
 	end
-  if current_file[1] ~= nil then
-    table.insert(file_list, current_file)
-  end
-
-  
+	if current_file[1] ~= nil then
+		table.insert(file_list, current_file)
+	end
 	return file_list
+end
+
+local get_file_ext = function(filename)
+	local ext = filename:match("^.+(%..+)$")
+	if ext == nil then
+		return ""
+	end
+	return ext
+end
+
+local get_file_icon = function(filename)
+	local ext = get_file_ext(filename)
+	print(filename)
+	print(ext)
+	-- TODO: figure out highlighting
+	ext = ext:sub(2)
+	local icon, icon_highlight = webdevicon.get_icon(filename, ext, { default = true })
+	return icon
 end
 
 -- { {
@@ -50,7 +67,7 @@ return function(opts)
 				entry_maker = function(entry)
 					return {
 						value = entry,
-						display = entry[1],
+						display = get_file_icon(entry[1]) .. " " .. entry[1]:sub(2) .. ":" .. entry[2],
 						ordinal = tostring(entry[1]),
 						path = entry[4] .. entry[1],
 						lnum = entry[2],
